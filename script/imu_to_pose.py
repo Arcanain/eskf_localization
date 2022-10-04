0439b364f229bf7e25f8a409a6617f7ec725e026
Ramune6110 <arduino6110@gmail.com>
2022-10-05 02:26:27 +0900

    [modify]lat0 lon0

---
+++ script/imu_to_pose.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import Imu
+
+def imu_callback(imu_msg):
+    ps_msg = PoseStamped()
+    ps_msg.header=imu_msg.header
+    ps_msg.pose.orientation=imu_msg.orientation
+    posestamped_pub.publish(ps_msg)
+
+if __name__ == "__main__":
+    rospy.init_node("imu_to_pose")
+    #Publisher
+    posestamped_pub = rospy.Publisher("imu_pose", PoseStamped, queue_size=10)
+    #Subscriber
+    rospy.Subscriber("imu", Imu, imu_callback)
+    rospy.spin()
+
--- src/eskf_localization.cpp
+++ src/eskf_localization.cpp
@@ -12,8 +12,12 @@ int main(int argc, char**argv)
     double lon0 = 8.899999999175634;*/
 	
     // utbm_robocar_dataset_20190131_noimage.bag
-    double lat0 = 47.5115140833;
-    double lon0 = 6.79310693333;
+    /*double lat0 = 47.5115140833;
+    double lon0 = 6.79310693333;*/
+
+    // hirayama 
+    double lat0 = 35.056373;
+    double lon0 = 137.171547;
 
     ROS_Interface ros_interface(n, lat0, lon0);
 
